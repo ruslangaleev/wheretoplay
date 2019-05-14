42ba93fb6226af76f09f5d66328af4ec0c2f7e70
Руслан Галеев <ruslan.nafisovich@gmail.com>
2019-05-14 17:08:46 +0300

    Updates

---
+++ precache-manifest.e40409f85f8e3fd47f07da189315c294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39667f1f3a3812a65231",
+    "url": "/wheretoplay/static/js/main.39667f1f.chunk.js"
+  },
+  {
+    "revision": "1a74402c8b228651091b",
+    "url": "/wheretoplay/static/js/runtime~main.1a74402c.js"
+  },
+  {
+    "revision": "dd90cbae3fe35332e970",
+    "url": "/wheretoplay/static/css/2.d25c0602.chunk.css"
+  },
+  {
+    "revision": "dd90cbae3fe35332e970",
+    "url": "/wheretoplay/static/js/2.dd90cbae.chunk.js"
+  },
+  {
+    "revision": "b98e8522c03b8be56420785882be4ddb",
+    "url": "/wheretoplay/index.html"
+  }
+];
